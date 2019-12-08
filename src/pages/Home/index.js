48cdad2b0b5d2c5894256fcd3896c16122a31dc8
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-08 13:40:06 -0300

    Layout da página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,45 @@
 import React from 'react';
-
-import { Container, HelloWorld } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { FlatList } from 'react-native';
+import {
+  Container,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  SubmitButton,
+  SubmitButtonAmount,
+  SubmitButtonAmountText,
+  SubmitButtonText,
+} from './styles';
 
 import Header from '../../components/Header';
+
+import { products } from '../../../api';
 
 export default function Home() {
   return (
     <Container>
-      <HelloWorld>Hello World!</HelloWorld>
+      <FlatList
+        vertical
+        data={products}
+        contentContainerStyle={{ alignItems: 'center' }}
+        keyExtractor={product => product.id.toString()}
+        renderItem={({ item }) => (
+          <Product>
+            <ProductImage source={{ uri: item.image }} />
+            <ProductTitle>{item.title}</ProductTitle>
+            <ProductPrice>R$ 179.90</ProductPrice>
+            <SubmitButton>
+              <SubmitButtonAmount>
+                <Icon name="add-shopping-cart" color="white" size={24} />
+                <SubmitButtonAmountText>0</SubmitButtonAmountText>
+              </SubmitButtonAmount>
+              <SubmitButtonText>ADICIONAR</SubmitButtonText>
+            </SubmitButton>
+          </Product>
+        )}
+      />
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,14 +1,62 @@
 import styled from 'styled-components/native';
 
+import { darken } from 'polished';
+
+import colors from '../../styles/colors';
+
 export const Container = styled.View`
   background: #191920;
-  flex: 1;
-  justify-content: center;
+`;
+
+export const Product = styled.View`
+  background: #fff;
+  width: 350px;
+  border-radius: 4px;
+  padding: 12px;
+  margin: 15px;
+`;
+
+export const ProductImage = styled.Image`
+  height: 220px;
+  width: 220px;
+  margin: auto;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 18px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  margin: 16px 0;
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  background: ${colors.primary};
+  flex-direction: row;
+  border-radius: 4px;
+  align-items: center;
+  margin-top: auto;
+`;
+
+export const SubmitButtonAmount = styled.View`
+  background: ${darken(0.05, colors.primary)};
+  padding: 12px;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  flex-direction: row;
   align-items: center;
 `;
 
-export const HelloWorld = styled.Text`
-  font-size: 20px;
+export const SubmitButtonAmountText = styled.Text`
   color: #fff;
+  margin: 0px 4px 0px 10px;
+`;
+
+export const SubmitButtonText = styled.Text`
+  flex: 1;
+  text-align: center;
   font-weight: bold;
+  color: #fff;
 `;
