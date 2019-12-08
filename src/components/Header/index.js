2374172abd22dbaccfb13227578971be7407454e
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-08 17:48:25 -0300

    Navegação no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Logo, Basket, ItemCount } from './styles';
 
-export default function Header() {
-  return (
-    <Container>
-      <Logo />
-      <Basket>
-        <Icon name="shopping-basket" color="white" size={24} />
-        <ItemCount>{0}</ItemCount>
-      </Basket>
-    </Container>
-  );
+export default class Header extends Component {
+  handleBasketNavigate = () => {
+    const { navigation } = this.props;
+    navigation.navigate('Cart');
+  };
+
+  handleLogoNavigate = () => {
+    const { navigation } = this.props;
+    navigation.navigate('Home');
+  };
+
+  render() {
+    return (
+      <Container>
+        <TouchableOpacity onPress={() => this.handleLogoNavigate()}>
+          <Logo />
+        </TouchableOpacity>
+        <Basket onPress={() => this.handleBasketNavigate()}>
+          <Icon name="shopping-basket" color="white" size={24} />
+          <ItemCount>{0}</ItemCount>
+        </Basket>
+      </Container>
+    );
+  }
 }
